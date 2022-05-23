31a1746b3dc9676095fd948bf6f354c3c280bd5b
Learny <learny@Learnys-Mac-mini.local>
2022-05-23 18:08:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// let username;
+// document.getElementById("mybutton").onclick=function(){
+// username = document.getElementById("my text").Value;
+// console.log(username);
+//   document.getElementById("my label").innerHTML="hello"
+
+
+// let age = prompt("how old are you ?");
+
+// age +=1;
+// console.log("H B ",age,"years old");
+
+// let age
+
+// let a;
+// let b;
+// let c;
+
+// a= prompt("enter side A");
+// a=Number(a);
+
+// b=prompt("enter side B");
+// b=Number(b);
+
+// c=Math.sqrt(Math.pow(a,2)+ Math.pow(b,2));
+
+// // console.log("side c:",c);
+// document.getElementById("submitbutton").onclick=function(){
+
+// a=document.getElementById("atextbox").value;
+// a= Number(a);
+// b=document.getElementById("btextbox").value;
+// b= Number(b);
+// c= Math.sqrt(Math.pow (a,2)+ Math.pow(b,2));
+
+// document.getElementById("clabel").innerHTML ="side c: "+c;
+
+// }
+// let count=0;
+//       document.getElementById("decrease button").onclick = function (){
+//           count-=1;
+//     document.getElementById("countable").innerHTML=count;
+//  }
+
+//  document.getElementById("reset button").onclick = function (){
+//           count=0;
+// document.getElementById("countable").innerHTML=count;
+//  }
+
+
+//  document.getElementById("increase button").onclick = function (){
+//             count+=1;
+// document.getElementById("countable").innerHTML=count;
+// }
+                                                     
+
+
